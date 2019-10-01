8dfd63e9d5e706b74a56fec754281ba003d51a01
simon kabir <sksimonkabir@gmail.com>
2019-10-01 07:36:41 +0100

    added a new method called greeting to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(Sting[] args) {
         system.out.println("vscode is awesome...")
+
+        greeting("BB Simon")
+    }
+
+    public static void greeting(string name) {
+        system.out.println("Welcome" + name)
     }
 }
